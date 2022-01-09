e1b10ccfcf55a35d033b2daba7e1c3b18e0bbd61
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-01-09 03:52:23 +0100

    add: header to docker-compose-manager.sh

--- docker-compose-manager.sh
+++ docker-compose-manager.sh
@@ -14,14 +14,16 @@ array+=( "Quit" )
 #array=($(ls -d */))
 #array+=( "Quit" )
 
-function print_menu()  # selected_item, ...menu_items
+function print_menu()  # header, selected_item, ...menu_items
 {
 	local function_arguments=($@)
-
-	local selected_item="$1"
-	local menu_items=(${function_arguments[@]:1})
+	local header="$1"
+	local selected_item="$2"
+	local menu_items=(${function_arguments[@]:2})
 	local menu_size="${#menu_items[@]}"
 
+  echo "############### docker-compose Manager ###############"
+  echo -e "############### $header ###############\n"
 	for (( i = 0; i < $menu_size; ++i ))
 	do
 		if [ "$i" = "$selected_item" ]
@@ -36,14 +38,14 @@ function print_menu()  # selected_item, ...menu_items
 function run_menu()  # selected_item, ...menu_items
 {
 	local function_arguments=($@)
-
-	local selected_item="$1"
-	local menu_items=(${function_arguments[@]:1})
+	local header="$1"
+	local selected_item="$2"
+	local menu_items=(${function_arguments[@]:2})
 	local menu_size="${#menu_items[@]}"
 	local menu_limit=$((menu_size - 1))
 
 	clear
-	print_menu "$selected_item" "${menu_items[@]}"
+	print_menu "$header" "$selected_item" "${menu_items[@]}"
 
 	while read -rsn1 input
 	do
@@ -61,11 +63,11 @@ function run_menu()  # selected_item, ...menu_items
 							then
 								selected_item=$((selected_item - 1))
 								clear
-								print_menu "$selected_item" "${menu_items[@]}"
+								print_menu "$header" "$selected_item" "${menu_items[@]}"
 							else
 							  selected_item=$((menu_limit - 1))
 							  clear
-							  print_menu "$selected_item" "${menu_items[@]}"
+							  print_menu "$header" "$selected_item" "${menu_items[@]}"
 							fi
 							;;
 						B)  # Down Arrow
@@ -73,11 +75,11 @@ function run_menu()  # selected_item, ...menu_items
 							then
 								selected_item=$((selected_item + 1))
 								clear
-								print_menu "$selected_item" "${menu_items[@]}"
+								print_menu "$header" "$selected_item" "${menu_items[@]}"
               else # selected_item = menu_limit
                 selected_item=0
                 clear
-                print_menu "$selected_item" "${menu_items[@]}"
+                print_menu "$header" "$selected_item" "${menu_items[@]}"
 							fi
 							;;
 					esac
@@ -92,8 +94,9 @@ function run_menu()  # selected_item, ...menu_items
 }
 
 selected_item=0
+menu_header1="choose a folder: "
 echo "Select the folder: "
-run_menu "$selected_item" "${array[@]}"
+run_menu "$menu_header1" "$selected_item" "${array[@]}"
 menu_result="${array[$?]}"
 folder=$menu_result
 echo $menu_result
@@ -105,10 +108,20 @@ then
     exit 0
 fi
 
+
+
+menu_header2=""
+if [ -z "cd $folder docker-compose ps" ]
+then
+  menu_header2 = "$folder: down, choose an operation: "
+else
+  menu_header2 = "$folder: up, choose an operation: "
+fi
+
 selected_item1=0
 array1=("up" "down" "restart" "resync" "Quit")
 echo "Select the operation: "
-run_menu "$selected_item1" "${array1[@]}"
+run_menu "$menu_header2" "$selected_item1" "${array1[@]}"
 menu_result1="${array1[$?]}"
 operation=$menu_result1
 echo $menu_result1
